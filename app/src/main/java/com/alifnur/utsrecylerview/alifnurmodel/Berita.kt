27e12cf6834f8da7d2523a9404e757ad0907e0d8
alifnur0020 <101784676+alifnur0020@users.noreply.github.com>
2022-04-04 01:08:58 +0700

    Update Berita.kt

--- app/src/main/java/com/alifnur/utsrecylerview/alifnurmodel/Berita.kt
+++ app/src/main/java/com/alifnur/utsrecylerview/alifnurmodel/Berita.kt
@@ -2,7 +2,7 @@ package com.alifnur.utsrecylerview.alifnurmodel
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-
+//class berita
 data class Berita(
     @StringRes val stringResourceId: Int,
     @DrawableRes val imageResourceId: Int
