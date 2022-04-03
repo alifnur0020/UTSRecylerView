bb802419de74adb9dc702197b4821104116dcae4
alifnur0020 <101784676+alifnur0020@users.noreply.github.com>
2022-04-04 01:07:14 +0700

    Update ItemAdapter.kt

--- app/src/main/java/com/alifnur/utsrecylerview/alifnuradapter/ItemAdapter.kt
+++ app/src/main/java/com/alifnur/utsrecylerview/alifnuradapter/ItemAdapter.kt
@@ -10,26 +10,21 @@ import androidx.recyclerview.widget.RecyclerView
 import com.alifnur.utsrecylerview.R
 import com.alifnur.utsrecylerview.alifnurmodel.Berita
 
-/**
- * Adapter digunakan untuk [RecyclerView] di [MainActivity]. Menampilkan [Beritaa] sebagai data object.
- */
+//Adapter digunakan untuk [RecyclerView] di [MainActivity]. Menampilkan [Berita] sebagai data object.
+ 
 class ItemAdapter(
     private val context: Context,
     private val dataset: List<Berita>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder.
-    // Each data item is just an Affirmation object.
+    // memberikan sumber dari setiap data item
     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
         val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
-    /**
-     * Membuat tampilan baru
-     */
+    // Membuat tampilan baru
+     
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         // create a new view
         val adapterLayout = LayoutInflater.from(parent.context)
@@ -38,17 +33,15 @@ class ItemAdapter(
         return ItemViewHolder(adapterLayout)
     }
 
-    /**
-     * Untuk mengubah tampilan konten
-     */
+    //Untuk mengubah tampilan konten
+    
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
         holder.imageView.setImageResource(item.imageResourceId)
     }
 
-    /**
-     * Mengembalikan ukuran dataset
-     */
+    //Mengembalikan ukuran dataset
+
     override fun getItemCount() = dataset.size
 }
